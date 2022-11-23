cdfebcbeea09ccb6eb1c8dd5f986fa2b24f7bcaa
CALVINCE <calvince.ogwel@gmail.com>
2022-11-23 22:27:31 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+function divide(num1 = 2000, num2 = 100) {
+    return num1/num2
+}
+
+function square(number = 2) {
+    return number*number
+}
+
+function add(num1 = 4, num2 = 3) {
+    return num1+num2
+}
